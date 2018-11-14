bb8f51293f8096dd7e469d0363fab2fad58e3120
florent champigny <florent.champigny@backelite.com>
2018-11-14 12:22:48 +0100

    v41

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 ./gradlew clean
 ./gradlew build
-./gradlew publishToMavenLocal
-./gradlew bintrayUpload
+./gradlew :multiplatform-log-ios:publishToMavenLocal
+./gradlew :multiplatform-log-android:publishToMavenLocal
+./gradlew :publishToMavenLocal
+#./gradlew bintrayUpload
